80f9adbfe201277d3f305203fa04dc1d5ab11012
brenovogt <breno.vogt.souza@escola.pr.gov.br>
2024-10-22 10:49:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,50 @@
+criaCartao(
+    'RAP',
+    'Quem é o  Slim Shady?',
+    'eminem'
+)
+criaCartao(
+    'RAP',
+    ' Qual álbum do Racionais mais fez sucesso?',
+    'Sobrevivendo no inferno'
+)
+
+criaCartao(
+    'RAP',
+    'Qual é o nome da gravadora do snoop dogg?',
+    'Death Row Records',
+)
+
+criaCartao(
+    'RAP',
+    'Qual musica o eminem fez para a filha dele?',
+    'Mockingbird',
+)
+
+criaCartao(
+    'RAP',
+    'Qual o álbum mais famoso do Orochi?',
+    'Celebridade',
+)
+
+
+criaCartao(
+    'RAP',
+    'Qual o cantor de Rap mais icônico do Brasil?',
+    'Sabotagem',
+)
+criaCartao(
+  'RAP',
+  'Qual o maior sucesso de Tupac?',
+  '"How Long Will They Mourn Me?"'
+)
+criaCartao(
+  'RAP',
+  'Quem é o deus do rap?',
+  'EMINEM'
+)
+criaCartao(
+  'RAP',
+  'Quem fazia parte da NWA?',
+  'Easy-E, Dr. Dre, Ice Cube, MC Ren e DJ Yella'
+)
